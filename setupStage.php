03bb067f606a9e1f26f2ef984b51efa3ba9d2e5f
Vasyl Martiiashenko <vmartiyashenko@gorillagroup.com>
2015-07-06 14:37:46 -0400

    Create function for setup config value for all stores/websites, add update of GA

--- setupStage.php
+++ setupStage.php
@@ -3,12 +3,41 @@
  * Created by PhpStorm.
  * User: vmart_000
  * Date: 7/3/2015
- * Time: 1:09 PM
+ * Time: 1:09 PM web/browser_capabilities/cookies
  */
 require_once 'abstract.php';
 
 class Gorilla_SetupStage extends Mage_Shell_Abstract {
+    /**
+     * ID of test account used for GA on stage servers
+     */
     const GOOGLE_ANALYTHICS_ACCOUN_ID = 'test';
+
+    /**
+     * Path for GA account in core_config_data
+     */
+    const XML_PATH_GOOGLE_ANALYTICS = 'google/analytics/account';
+
+    /**
+     * Websites cache
+     *
+     * @var array
+     */
+    protected $_websites = null;
+
+    /**
+     * Stores cache
+     *
+     * @var array
+     */
+    protected $_stores = null;
+
+    /**
+     * Config Model Cache
+     *
+     * @var Mage_Core_Model_Config
+     */
+    protected $_configModel = null;
 
     /**
      * Run script
@@ -30,8 +59,11 @@ class Gorilla_SetupStage extends Mage_Shell_Abstract {
 
     }
 
+    /**
+     * Update Google Analytics account in order to prevent tracking test data into real account
+     */
     protected function setGA(){
-
+        $this->setConfigGlobally(self::XML_PATH_GOOGLE_ANALYTICS, self::GOOGLE_ANALYTHICS_ACCOUN_ID);
     }
 
     protected function removeCookieDomain() {
@@ -56,6 +88,69 @@ class Gorilla_SetupStage extends Mage_Shell_Abstract {
 
     protected function removeCustomersData() {
 
+    }
+
+    /**
+     * Remove config for all websites/stores and set one global value
+     *
+     * @param string $config
+     * @param string $value
+     */
+    protected function setConfigGlobally($config, $value){
+        $websites = $this->getWebsites();
+        $stores = $this->getStores();
+
+        $configModel = $this->getConfigModel();
+
+        foreach($websites as $website){
+            $configModel->deleteConfig($config, 'websites', $website->getId());
+        }
+
+        foreach($stores as $store) {
+            $configModel->deleteConfig($config, 'stores', $store->getId());
+        }
+
+        $configModel->saveConfig($config, $value);
+    }
+
+    /**
+     * Clear cache and reload configuration
+     */
+    protected function applyChanges() {
+        Mage::getConfig()->reinit();
+        Mage::app()->reinitStores();
+    }
+
+    /**
+     * Get all websites
+     */
+    protected function getWebsites(){
+        if($this->_websites === null){
+            $this->_websites = Mage::app()->getWebsites();
+        }
+
+        return $this->_websites;
+    }
+
+    /**
+     * Get all stores
+     */
+    protected function getStores() {
+       if($this->_stores === null) {
+           $this->_stores = Mage::app()->getStores();
+       }
+
+       return $this->_stores;
+    }
+
+    /**
+     * Get object of Mage_Core_Model_Config
+     */
+    protected function getConfigModel(){
+        if($this->_configModel ===  null) {
+            $this->_configModel = Mage::getModel('core/config');
+        }
+        return $this->_configModel;
     }
 }
 
